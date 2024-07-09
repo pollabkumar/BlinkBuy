87915a800b0f3f5075635fcef125476f3d4bc582
pollabkumar <pollabkumar777@gmail.com>
2024-07-09 22:19:18 +0530

    pollab

--- src/components/MainPage/Home.jsx
+++ src/components/MainPage/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <>
       <section className='home'>
-        <div className='container d_flex'>
+        <div className='container '>
           {/* <Categories /> */}
           <SliderHome />
         </div>
--- src/components/MainPage/Slider.jsx
+++ src/components/MainPage/Slider.jsx
@@ -4,7 +4,7 @@ import SlideCard from "./SlideCard"
 const SliderHome = () => {
   return (
     <>
-      <section className='homeSlide contentWidth'>
+      <section className='homeSlide '>
         <div className='container'>
           <SlideCard />
         </div>
